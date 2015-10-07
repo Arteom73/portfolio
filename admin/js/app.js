417390f1f7112ba5a31cd2bc2ddb54b43ec7af96
Артем Овчинников <artem9573@mail.ru>
2015-10-07 14:38:19 +0300

    add approve button

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,6 +1,6 @@
 $(function() {
 		
-
+	// Ajax approve/disprove reviews
 	$('.js-approve-button').on('click', function(e) {
 
 		e.preventDefault();
@@ -18,7 +18,55 @@ $(function() {
 
 		})
 
-	})
+	});
 
+	// Ajax edit review
+	$('.js-edit-button').on('click', function(e) {
+
+		e.preventDefault();
+
+		$.ajax({
+
+			type: 'post',
+			url: '/admin/ajax/reviews_edit.php',
+			data: {
+				id: Number($(this).closest('.js-approve-item').data('id'))
+			},
+			success: function(date) {
+				doModal('asd','asd','asd');
+			}
+
+		})
+
+	});
+
+	function doModal(placementId, heading, formContent, strSubmitFunc, btnText) {
+	    html =  '<div id="dynamic-modal" class="modal fade in" style="display:none;">';
+	    html += '<div class="modal-dialog">';
+	    html += '<div class="modal-content">';
+	    html += '<div class="modal-header">';
+	    html += '<a class="close" data-dismiss="modal">×</a>';
+	    html += '<h4>'+heading+'</h4>'
+	    html += '</div>';
+	    html += '<div class="modal-body">';
+	    html += '<p>';
+	    html += formContent;
+	    html += '</div>';
+	    html += '<div class="modal-footer">';
+	    if (btnText!='') {
+	        html += '<span class="btn btn-success"';
+	        html += ' onClick="'+strSubmitFunc+'">'+btnText;
+	        html += '</span>';
+	    }
+	    html += '<span class="btn" data-dismiss="modal">x</span>'; // close button
+	    html += '</div>';  // footer
+	    html += '</div>';  // modalWindow
+	    html += '</div>';  // modalWindow
+	    html += '</div>';  // modalWindow
+	    $(html).appendTo($('body'));
+	    $("#dynamic-modal").modal();
+	    $("#dynamic-modal").on('hidden.bs.modal', function (e) {
+			$(this).remove();
+		});
+	}
 })
-console.log('asd');
--- admin/sections/reviews/show.index.php
+++ admin/sections/reviews/show.index.php
@@ -4,12 +4,18 @@ $sql = 'SELECT * FROM reviews ORDER BY id';
 $html = '';
 
 foreach ($db->query($sql) as $row) {
-    $html .= '<div class="panel panel-default js-approve-item" data-id="'.$row['id'].'">';
+    $html .= '<div class="panel panel-default js-approve-item js-edit-item" data-id="'.$row['id'].'" id="review-'.$row['id'].'">';
     $html .= '<div class="panel-heading">';
-    $html .= '<a class="js-approve-button pull-right" href="#">Approve</a>';
+    $html .= '<a class="js-approve-button pull-right" href="#">';
+    if ($row['approve'] == 0) {
+	    $html .= 'Approve';
+	} else {
+	    $html .= 'Disprove';
+	}
+    $html .= '</a>';
     $html .= 'Author: '.$row['author'].' | Date: '.$row['date'].' | IP: ' .$row['ip'].' | Company: ' .$row['company'].' | Approve: ' .$row['approve'];
     $html .= '</div>';
-    $html .= '<div class="panel-body">'.$row['message'].'</div>';
+    $html .= '<div class="panel-body js-edit-button">'.$row['message'].'</div>';
     $html .= '</div>';
 }
 ?>
